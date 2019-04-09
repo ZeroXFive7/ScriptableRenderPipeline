b0bd6acec802a6035e5dc2b50667df3e46c9a488
Josh Wehrly <josh.wehrly@gmail.com>
2019-04-09 08:53:25 -0700

    First/third person components now enable/disable renderers as a function of the actively rendering camera.  This enables support for split screen multiplayer.

--- com.unity.render-pipelines.lightweight/Runtime/LWRPFirstPersonRenderer.cs
+++ com.unity.render-pipelines.lightweight/Runtime/LWRPFirstPersonRenderer.cs
@@ -1,19 +1,57 @@
-using UnityEngine;
-
 namespace UnityEngine.Experimental.Rendering.LightweightPipeline
 {
     public class LWRPFirstPersonRenderer : MonoBehaviour
     {
+        #region Fields
+
+        private new Camera camera = null;
         private Renderer[] renderers = null;
+
+        #endregion
+
+        #region Properties
+
+        public Camera Camera { set { camera = value; } }
+
+        #endregion
+
+        #region Mono Messages
 
         private void Awake()
         {
             renderers = GetComponentsInChildren<Renderer>();
+
             foreach (var renderer in renderers)
             {
                 renderer.renderingLayerMask = 2;
                 renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
             }
         }
+
+        private void OnEnable()
+        {
+            LightweightRenderPipeline.BeforeCameraRenderEvent += OnBeforeCameraRender;
+        }
+
+        private void OnDisable()
+        {
+            LightweightRenderPipeline.BeforeCameraRenderEvent -= OnBeforeCameraRender;
+        }
+
+        #endregion
+
+        #region Camera Callbacks
+
+        private void OnBeforeCameraRender(Camera camera)
+        {
+            var visibleInThisCamera = this.camera != null && this.camera == camera;
+
+            foreach (var renderer in renderers)
+            {
+                renderer.enabled = visibleInThisCamera;
+            }
+        }
+
+        #endregion
     }
 }
